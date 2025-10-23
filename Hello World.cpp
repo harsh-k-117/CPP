4a9cbeb2517a6dc385b5b4e0a54a6682542fb19f
harsh-k-117 <kulkarniharsh117@gmail.com>
2025-10-23 12:12:20 +0530

    Easy committed changes to Hello World.cpp

--- Hello World.cpp
+++ Hello World.cpp
@@ -3,19 +3,6 @@ using namespace std;
 
 int main()
 {
-    int a, b, sum, sub, mul, div, mod;
-    cout << "Enter any two numbers: " << endl;
-    cin >> a >> b;
-
-    sum = a + b;
-    sub = a - b;
-    mul = a * b;
-    div = a / b;
-    mod = a % b;
-
-    cout << "Addition is " << sum << "\nSubtraction is " << sub
-         << "\nMultiplication is " << mul << "\nDivision is " << div
-         << "\nRemainder is " << mod << endl;
-
+    cout << "Hello, World!" << endl;
     return 0;
 }
