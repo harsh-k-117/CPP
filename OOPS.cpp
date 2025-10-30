32bc16a1e2dc4f817dc98b7d9148e1e221191335
harsh-k-117 <kulkarniharsh117@gmail.com>
2025-10-30 19:18:15 +0530

    Modified files staged for commit

--- OOPS.cpp
+++ OOPS.cpp
@@ -9,17 +9,33 @@ private:       //data hiding
     float salary;
 
 public:
-
     string name;
     string dept;
     string subject;
-    
+
+    /*Teacher(){    // non parameterized constructor
+        cout << "Teacher Object Created" << endl;
+    }
+    */ 
+
+    Teacher(Teacher &orgObj){  // Copy Constructor
+        cout<<"Copy Constructor Called" << endl;
+        this->name = orgObj.name;
+        this->dept = orgObj.dept;   
+        this->subject = orgObj.subject; 
+    }
+
+    Teacher(string name, string dept, string subject){   // parameterized constructor
+        this->name = name;
+        this->dept = dept;
+        this->subject = subject;
+    }
 
     // Methods or behaviors or member functions
     void ChangeDept(string newdept){
         dept = newdept;
     }
-
+/*
     // Setter for salary
     void setSalary(float s){
         salary = s;
@@ -29,16 +45,19 @@ public:
     float getSalary(){
         return salary;
     }
+*/
+    void getInfo(){
+        cout << "Name: " << name << endl;
+        cout << "Department: " << dept << endl;
+        cout << "Subject: " << subject << endl;
 
+    }
 };
 
 int main(){
     // Create an object of Teacher class
-    Teacher t1; //constructor called
-    t1.name = "Mr. Kulkarni";
-    t1.dept = "Computer Science";
-    t1.subject = "Data Structures";
-
+    Teacher t1("Mr. Kulkarni", "Computer Science", "Data Structures"); //parameterized constructor called
+/*
     cout << "Teacher Name: " << t1.name << endl;
     cout << "Department: " << t1.dept << endl;
     cout << "Subject: " << t1.subject << endl;  
@@ -48,6 +67,11 @@ int main(){
 
     t1.setSalary(60000);
     cout << "Salary: " << t1.getSalary() << endl;   
+*/
+    //t1.getInfo();
+
+    Teacher t2(t1); // Copy constructor called
+    t2.getInfo();
 
     return 0;
 }
