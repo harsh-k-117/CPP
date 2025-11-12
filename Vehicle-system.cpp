c16790abf0ab52fce9ccb869b83119c4bdcecdbd
harsh-k-117 <kulkarniharsh117@gmail.com>
2025-11-12 21:41:21 +0530

    NA

--- Vehicle-system.cpp
+++ Vehicle-system.cpp
@@ -5,33 +5,18 @@ using namespace std;
 class Vehicle {
 protected:
 
-    string company, model, Fueltype;
+    string company, model;
     double mileage, price;
 
-public:
-
-    void Input(){
-        cout << "Enter Vehicle Company: ";
-        cin >> company;
-        cout << "Enter Vehicle Model: ";
-        cin >> model;
-        cout << "Enter Vehicle Fuel Type: ";
-        cin >> Fueltype;
-        cout << "Enter Vehicle Mileage: ";
-        cin >> mileage;
-        cout << "Enter Vehicle Price: ";
-        cin >> price;
-    }
-
-    void Display(){
-        cout << "Vehicle Company: " << company << endl;
-        cout << "Vehicle Model: " << model << endl;
-        cout << "Vehicle Fuel Type: " << Fueltype << endl;
-        cout << "Vehicle Mileage: " << mileage << " km/l" << endl;
-        cout << "Vehicle Price: $" << price << endl;
-    }
-};
-
-class Car : public Vehicle{
+    class Registration{
+        string regNumber;
+        string ownerName;   
+        string regDate;
+    public:
+    };
     
-}
+public:
+    
+
+
+};
